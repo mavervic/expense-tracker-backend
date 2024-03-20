60bdcf6658f3f718d28858c00d66d104fc4925a1
Vic.Liu <sunset1117@hotmail.com>
2024-03-20 21:11:01 +0800

    feat: update connection

--- server.js
+++ server.js
@@ -5,31 +5,53 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
-// 創建MySQL連接池
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "123456",
-  database: "demo_db",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+// // 創建MySQL連接池
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "123456",
+//   database: "demo_db",
+//   // database: "expense_tracker",
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
 
-app.get("/", (req, res) => {
-  res.send("Hello from Express!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello from Express!");
+// });
 
 app.get("/db", async (req, res) => {
+  const sql = req.query.sql; // Extract SQL query from query string
+  if (!sql.toLowerCase().startsWith("select")) {
+    return res.status(400).send({ error: "Only SELECT queries are allowed" });
+  }
+
+  console.log("sql", sql);
+
+  // replace \n with space
+  // const formattedSql = sql.replace(/\n/g, " ");
+  // console.log("formattedSql", formattedSql);
+
   try {
     // 從連接池中獲取一個連接
-    const connection = await pool.getConnection();
+    // const connection = await pool.getConnection();
+
+    const connection = await mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "123456",
+      database: req.query.dbName || "demo_db",
+    });
 
     // 執行SQL查詢
-    const [rows, fields] = await connection.execute("SELECT * FROM mytable");
+    const [rows, fields] = await connection.execute(sql);
+    console.log(rows);
 
     // 釋放連接
-    connection.release();
+    // connection.release();
+
+    await connection.end();
 
     // 返回查詢結果
     res.json(rows);
